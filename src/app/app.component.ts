442dc2f4b3c6db6bd591b940ddcb461f4b6aa6bd
Riccardo Manzan <riccardo.manzan.dev@gmail.com>
2023-02-18 20:29:24 +0100

    dev: introduced some GUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'afds-dono-cup-leaderboard-generator';
+  ENTRYPOINT = 0;
+  LOAD = 1;
+  RESULTS = 2;
+  step: number = this.ENTRYPOINT;
+
+  reload() {
+    location.reload();
+  }
 }
